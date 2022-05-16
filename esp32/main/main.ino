7149939c09401a21845f1eeb9d553baebf066046
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-16 03:11:28 -0300

    Script inicial ESP32

--- esp32/main/main.ino
+++ esp32/main/main.ino
@@ -6,17 +6,22 @@
  */
 HX711 scale;
 
+
+// 2. Adjustment settings
+const long LOADCELL_OFFSET = 50682624;
+const long LOADCELL_DIVIDER = 5895655;
+
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
   scale.begin(35, 34);
+  scale.set_scale(LOADCELL_DIVIDER);
+  scale.set_offset(LOADCELL_OFFSET);
   
   Serial.println("Mantenha a balança vazia");
   Serial.println("Tarando...");
   delay(3000);
-  Serial.print("Peso da prateleira: ");
-  Serial.println(scale.read_average(20));
   scale.tare();
 
 }
@@ -24,6 +29,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.println(scale.read_average(20));
+  Serial.println(scale.get_units(10), 2);
   delay(500);
 }
