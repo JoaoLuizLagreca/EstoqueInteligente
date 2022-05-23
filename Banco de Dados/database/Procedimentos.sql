9ec0579104bd98e6d10399388beadc541a2b508f
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-23 01:05:51 -0300

    Add Procedimentos.sql

---
+++ Banco de Dados/database/Procedimentos.sql
@@ -0,0 +1,44 @@
+use estoque_inteligente;
+
+delimiter $$
+create procedure adicionarProduto(NomeProduto varchar(50), Preco float, Peso_medio float)
+begin
+    insert into Produto(NomeProduto, Preco, peso_medio) values (
+        NomeProduto,
+        Preco,
+        Peso_medio
+    );
+end $$
+
+delimiter $$
+create procedure compra(produto_id integer, quantidade integer) 
+begin
+    if quantidade > 0 then
+        select @estoque_atual := estoque from Produto;
+
+        if @estoque_atual - quantidade < 0 then
+            # Relatar erro
+            signal SQLSTATE 'ERROR'
+            set MESSAGE_TEXT = 'Estoque seria inferior à 0 com essa quantidade';
+        else
+            update Produto set estoque = @estoque_atual - quantidade where id=produto_id;
+        end if;
+    else
+        # Relatar erro
+        signal SQLSTATE 'ERROR'
+        set MESSAGE_TEXT = 'Valor quantidade inválido';
+    end if;
+end $$
+
+delimiter $$
+create procedure reestocar(produto_id integer, quantidade integer)
+begin
+    if quantidade <= 0 then
+        # Relatar erro
+        signal SQLSTATE 'ERROR'
+        set MESSAGE_TEXT = 'Valor quantidade inválido';
+    else
+        select @estoque_atual := estoque from Produto;
+        update Produto set estoque = @estoque_atual + quantidade where id=produto_id;
+    end if;
+end $$
