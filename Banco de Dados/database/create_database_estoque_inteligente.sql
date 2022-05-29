bd890ed05de9f8ea00ba1bfb9db3f4d7637afb97
JessicaMinassidis <48689206+JessicaMinassidis@users.noreply.github.com>
2022-05-29 20:45:19 -0300

    alterações banco de dados (nomes de tabelas)

---
+++ Banco de Dados/database/create_database_estoque_inteligente.sql
@@ -0,0 +1,27 @@
+create database estoque_inteligente;
+use estoque_inteligente;
+
+create table produto(
+    produto_id Integer primary key auto_increment,
+    nome_produto varchar(50) not null unique,
+    preco float,
+    peso_medio float not null,
+    estoque integer default 0
+);
+
+create table prateleira(
+    id_prateleira integer primary key auto_increment,
+    quantidade integer not null default 0,
+    capacidade integer not null,
+    estado tinyint not null default 0,
+    produto integer not null,
+    foreign key (produto) references produto(produto_id)
+);
+
+create table log_de_estoque(
+    data_estocagem datetime not null default sysdate(),
+    quantidade integer not null,
+    preco float,
+    produto integer not null,
+    foreign key (produto) references produto(produto_id)
+);
